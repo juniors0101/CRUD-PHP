63abafeff13fb27acea8e4fc910b250ded22a628
junior sousa <92957053+juniors0101@users.noreply.github.com>
2022-10-06 08:44:57 -0300

    update header

--- header.php
+++ header.php
@@ -29,9 +29,7 @@
 						<li class="nav-item">
 							<a class="nav-link" href="?pagina=matriculas">Matrículas</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link disabled">Disabled</a>
-						</li>
+						
 						<?php if (isset($_SESSION['login'])) { ?>
 							<li class="nav-item">
 								<a class="nav-link" href="logout.php"><?php echo $_SESSION['usuario']; ?> (Sair)</a>
